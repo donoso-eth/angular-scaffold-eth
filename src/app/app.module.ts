44ef3e5b34f3a928eb64832223b3a3dc152787f0
jdonosocoding <javier.donoso@nuilea.com>
2022-01-19 18:54:01 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HelloWorldContractModule } from './dapp/demos/1-hello-world-contract/hello-world-contract.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HelloWorldContractModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
