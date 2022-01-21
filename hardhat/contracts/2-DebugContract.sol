d229967708ae9e79b09e3e9386edfdcb328b1f42
jdonosocoding <javier.donoso@nuilea.com>
2022-01-21 07:06:06 +0100

    debug

--- hardhat/contracts/2-DebugContract.sol
+++ hardhat/contracts/2-DebugContract.sol
@@ -1,33 +1,39 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9; // tells the solidity version to the complier
 
-import "hardhat/console.sol";
+// get the OpenZeppelin Contracts, we will use to creat our own
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract DebugContract {
-    string private greeting;
-    event ValueChangedEvent(uint256 value);
-    
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
-    }
+import "hardhat/console.sol"; // built in hardhat local environment 
 
-    
-    function greet() public view returns (string memory) {
-    
-        return greeting;
-    }
+contract NFTCollection is ERC721URIStorage {
 
-    function calculate(uint number1, uint number2) pure public returns (uint) {
-    return number1 * number2;
-    }
+// keep count of the tokenId
+using Counters for Counters.Counter; // keep track of the token id's
+Counters.Counter private _tokenIds;
 
-    function setGreeting(string memory _greeting) public {
-        greeting = _greeting;
-       
-    }
+uint256 public constant maxSupply = 2; // set the max supply of NFT's for your collection
 
-    receive() external payable {
-    emit ValueChangedEvent(msg.value);
+constructor() ERC721 ("NFTCollection Angular On Chain", "AOC") { // construct your token, needs name and symbol
+  console.log("An NFT has been minted to %s", msg.sender);
 }
+
+function createNFT() public { //function to create nfts
+
+  uint256 newItemId = _tokenIds.current(); // get the tokenId
+
+  require(newItemId < maxSupply); // check if the total supply has been reached 
+
+  _safeMint(msg.sender, newItemId); // mint the nft from the sender account 
+
+  _setTokenURI(newItemId, "https://jsonkeeper.com/b/2KQZ"); // add the contents to the nft 
+  // the content of this nft is on the url above. This means that the nft is an off-chain nft
+  // if the server with the content changes then the image in the url changes 
+
+  _tokenIds.increment(); // increment the token, so when the next person calls the function it will be the next token in line 
+
+  console.log("NFT ID %s has been minted", newItemId); 
+
+ }
 }
